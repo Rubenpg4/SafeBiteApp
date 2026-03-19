8583e3d05b53d415d9936e0e322d0e83708cfe76
Rubenpg4 <rubenprietogarcia10@gmail.com>
2026-03-19 03:07:14 +0100

    chore: secure firebase config with env variables

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "REMOVED_API_KEY",
-    authDomain: "safebite-f4915.firebaseapp.com",
-    projectId: "safebite-f4915",
-    storageBucket: "safebite-f4915.firebasestorage.app",
-    messagingSenderId: "856801488212",
-    appId: "1:856801488212:web:3ea2e0963649aca7ccecc1",
-    measurementId: "G-4P460XJ7P8"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "",
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "safebite-f4915.firebaseapp.com",
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "safebite-f4915",
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "safebite-f4915.firebasestorage.app",
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "856801488212",
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "1:856801488212:web:3ea2e0963649aca7ccecc1",
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-4P460XJ7P8"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
